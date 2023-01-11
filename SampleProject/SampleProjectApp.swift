0d6d80438209429d5f074097bdbefbf982cb1265
SagarikaSundiLogituit <Sagarika.sundi@logituit.com>
2023-01-11 22:50:37 +0530

    Initial Commit

---
+++ SampleProject/SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectApp.swift
+//  SampleProject
+//
+//  Created by CEPL on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
